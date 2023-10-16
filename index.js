01e023e81686d3a182bf89fe583cfaaa4073d21f
bh@nu77 <bhanupratap04123@gmail.com>
2023-10-16 13:54:56 +0530

    fix : cors problem

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const allowedOrigin = "https://todo-list-frontend-cw73.onrender.com";
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', allowedOrigin);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(204); // Pre-flight request. Respond successfully without further processing.
+    } else {
+        next(); // Continue processing the request.
+    }
+});
+
 const port = process.env.PORT | 1800;
 
 // Fetch data from database
