83e83aab5a4cda0f7b121d58f473c6e737e9f414
bh@nu77 <bhanupratap04123@gmail.com>
2023-10-16 18:43:29 +0530

    fix

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(cors(corsOption));
 //     });
 // };
 
-// const allowedOrigin = "https://todo-list-frontend-cw73.onrender.com";
+const allowedOrigin = "https://todo-list-frontend-cw73.onrender.com";
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', allowedOrigin);
