38af0d543efedbe729534a345b99cd160c70f4a5
bh@nu77 <bhanupratap04123@gmail.com>
2023-10-17 19:57:40 +0530

    fix : fix connection on mongodb cloud

--- db/config.js
+++ db/config.js
@@ -1,20 +1,28 @@
 
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
 // // mongoose.connect("mongodb://localhost:27017/todo-list");
 // // serverSelectionTimeoutMS: 2000,
 
-// mongoose.connect("mongodb+srv://bhanupratap04123:6cgUgOj0nOZm1g7m@cluster0.kf24ru9.mongodb.net/todo-list?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, maxPoolSize:10 })
-//     .then(() => {
-//         console.log('Connected to MongoDB');
-//     })
-//     .catch(error => {
-//         console.error('Error connecting to MongoDB:', error);
-//     });
+const mongooseOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 10000, // 10 seconds
+    socketTimeoutMS: 45000, // 45 seconds (adjust as needed)
+    connectTimeoutMS: 30000, // 30 seconds (adjust as needed)
+};
 
-import mongoose from "mongoose";
+mongoose.connect("mongodb+srv://bhanupratap04123:6cgUgOj0nOZm1g7m@cluster0.kf24ru9.mongodb.net/todo-list?retryWrites=true&w=majority", mongooseOptions)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(error => {
+        console.error('Error connecting to MongoDB:', error);
+    });
 
-mongoose.connect("mongodb+srv://bhanupratap04123:6cgUgOj0nOZm1g7m@cluster0.kf24ru9.mongodb.net/todo-list?retryWrites=true&w=majority");
+// import mongoose from "mongoose";
+
+// mongoose.connect("mongodb+srv://bhanupratap04123:6cgUgOj0nOZm1g7m@cluster0.kf24ru9.mongodb.net/todo-list?retryWrites=true&w=majority");
 
 // mongoose.connect("mongodb://localhost:27017/todo-list");
 
