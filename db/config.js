9f3369b4c65e02216ad1907568975c6092898ca0
bh@nu77 <bhanupratap04123@gmail.com>
2023-10-17 16:27:35 +0530

    fix

--- db/config.js
+++ db/config.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 // mongoose.connect("mongodb://localhost:27017/todo-list");
 
-mongoose.connect("mongodb+srv://bhanupratap04123:Dnd8lrlAWOeR59wX@cluster0.kf24ru9.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 15000, })
+mongoose.connect("mongodb+srv://bhanupratap04123:yLwfk5Yn2KcluSgP@cluster0.kf24ru9.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 15000, })
     .then(() => {
         console.log('Connected to MongoDB');
     })
