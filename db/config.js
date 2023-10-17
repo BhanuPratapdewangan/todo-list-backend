8120fbdfd8f86a96d668f3f2ffb5092cf6685697
bh@nu77 <bhanupratap04123@gmail.com>
2023-10-17 16:51:10 +0530

    fix

--- db/config.js
+++ db/config.js
@@ -2,8 +2,8 @@
 import mongoose from "mongoose";
 
 // mongoose.connect("mongodb://localhost:27017/todo-list");
-
-mongoose.connect("mongodb+srv://bhanupratap04123:yLwfk5Yn2KcluSgP@cluster0.kf24ru9.mongodb.net/todo-list?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 2000, maxPoolSize:10 })
+// serverSelectionTimeoutMS: 2000,
+mongoose.connect("mongodb+srv://bhanupratap04123:yLwfk5Yn2KcluSgP@cluster0.kf24ru9.mongodb.net/todo-list?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, maxPoolSize:10 })
     .then(() => {
         console.log('Connected to MongoDB');
     })
